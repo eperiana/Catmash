3b30a6e8d9fc097c52a74fbd017f6b19cb2b5264
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-14 19:59:20 +0200

    feat: initial api commit

---
+++ api/src/test/java/fr/catmash/CatmashApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.catmash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
