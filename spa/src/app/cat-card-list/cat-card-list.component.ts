c20175d5653ddd7ff7839c3a367b84dbe9d07e32
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-15 14:52:28 +0200

    feat: création composant liste de carte

--- spa/src/app/cat-card-list/cat-card-list.component.ts
+++ spa/src/app/cat-card-list/cat-card-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { CatService } from '../services/cat.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cat-card-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cat-card-list.component.html',
   styleUrl: './cat-card-list.component.scss'
 })
-export class CatCardListComponent {
-
+export class CatCardListComponent{
+  private catService: CatService = inject(CatService);
+  
+  cats = this.catService.cats;
 }
