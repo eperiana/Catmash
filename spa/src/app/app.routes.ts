41f2c3e6a79b291f2c70d123f7f3a973c8492a8a
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-15 13:42:24 +0200

    feat: ajout routage

--- spa/src/app/app.routes.ts
+++ spa/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CatCardListComponent } from './cat-card-list/cat-card-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'hiscore', pathMatch: 'full'},
+    {path: 'hiscore', component: CatCardListComponent },
+    {path: '**', component: NotFoundComponent },
+];
