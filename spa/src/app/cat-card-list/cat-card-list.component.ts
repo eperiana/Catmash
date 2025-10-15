29804b0a1e97825ded6b83418a964bdbd55b68e4
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-15 13:42:15 +0200

    feat: ajout composant de liste de carte

---
+++ spa/src/app/cat-card-list/cat-card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-card-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-card-list.component.html',
+  styleUrl: './cat-card-list.component.scss'
+})
+export class CatCardListComponent {
+
+}
