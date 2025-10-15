103c045a9e2dd23977694b7ba507a85f3d61db51
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-15 14:51:33 +0200

    feat: mise à jour app component

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'spa';
-}
+export class AppComponent {}
