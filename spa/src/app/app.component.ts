96d09a8220cd4b6d9e5c4b3d820271d2d466f0fd
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-15 15:18:11 +0200

    feat: ajout badge trophé

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, FontAwesomeModule],
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faTrophy);
+  }
+}
