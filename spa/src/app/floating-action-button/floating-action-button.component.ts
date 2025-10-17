5d459f4a99550d0ea12d408ac92bcc0c58e4e461
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-17 11:46:03 +0200

    feat: ajout bouton d'action flottant

---
+++ spa/src/app/floating-action-button/floating-action-button.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatchService } from '../services/match.service';
+
+@Component({
+  selector: 'app-floating-action-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './floating-action-button.component.html',
+  styleUrl: './floating-action-button.component.scss'
+})
+export class FloatingActionButtonComponent {
+  private router = inject(Router);
+  private matchService = inject(MatchService);
+
+  /** mode = 'battle' | 'ranking' */
+  @Input() mode: 'battle' | 'ranking' = 'battle';
+
+  matchesPlayed = this.matchService.matchesPlayed; // signal
+
+  onClick() {
+    if (this.mode === 'battle') {
+      this.router.navigate(['/ranking']);
+    } else {
+      this.router.navigate(['/battle']);
+    }
+  }
+}
