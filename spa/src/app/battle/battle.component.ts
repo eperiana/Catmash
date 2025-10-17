ee19080eb3eed0aebfaa6f57753a3f2de9aa5313
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-17 11:46:39 +0200

    feat: ajout bouton flottant et système de chargement

--- spa/src/app/battle/battle.component.ts
+++ spa/src/app/battle/battle.component.ts
@@ -2,16 +2,19 @@ import { Component, effect, inject, OnInit, Signal, signal } from '@angular/core
 import { Cat } from '../models/cat';
 import { CatService } from '../services/cat.service';
 import { LoadingSpinnerComponent } from "../loading-spinner/loading-spinner.component";
+import { FloatingActionButtonComponent } from "../floating-action-button/floating-action-button.component";
+import { MatchService } from '../services/match.service';
 
 @Component({
   selector: 'app-battle',
   standalone: true,
-  imports: [LoadingSpinnerComponent],
+  imports: [LoadingSpinnerComponent, FloatingActionButtonComponent],
   templateUrl: './battle.component.html',
   styleUrl: './battle.component.scss'
 })
 export class BattleComponent implements OnInit {
-  private catService: CatService = inject(CatService);
+  private readonly catService: CatService = inject(CatService);
+  private readonly matchService: MatchService = inject(MatchService);
 
   readonly isLoading = signal<boolean>(true);
   
@@ -21,11 +24,9 @@ export class BattleComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading.set(false);
-    }, 1500)
+    }, 1000)
   }
   
- 
-
   vote(winner: 'left' | 'right') {
     const chosen = winner === 'left' ? this.leftCat() : this.rightCat();
     if (chosen) {
@@ -33,9 +34,10 @@ export class BattleComponent implements OnInit {
         next: () => {
           this.catService.pickRandomCats();
           this.isLoading.set(true);
+          this.matchService.incrementMatch();
           setTimeout(() => {
             this.isLoading.set(false);
-          }, 1500)
+          }, 1000)
         },
         error: () => {
           console.log("API ERROR")
--- spa/src/app/cat-card-list/cat-card-list.component.ts
+++ spa/src/app/cat-card-list/cat-card-list.component.ts
@@ -1,18 +1,28 @@
-import { Component, effect, inject, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, Signal, signal, WritableSignal } from '@angular/core';
 import { CatService } from '../services/cat.service';
 import { CommonModule } from '@angular/common';
 import { CatCardComponent } from "../cat-card/cat-card.component";
+import { FloatingActionButtonComponent } from "../floating-action-button/floating-action-button.component";
+import { Cat } from '../models/cat';
 import { LoadingSpinnerComponent } from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-cat-card-list',
   standalone: true,
-  imports: [CommonModule, CatCardComponent, LoadingSpinnerComponent],
+  imports: [CommonModule, CatCardComponent, FloatingActionButtonComponent, LoadingSpinnerComponent],
   templateUrl: './cat-card-list.component.html',
   styleUrl: './cat-card-list.component.scss'
 })
-export class CatCardListComponent{
-  private catService: CatService = inject(CatService);
+export class CatCardListComponent implements OnInit{
+
+  private readonly catService: CatService = inject(CatService);
+  readonly isLoading: WritableSignal<boolean> = signal<boolean>(true);
+  cats: Signal<Cat[]> = this.catService.cats;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1000);
+  }
   
-  cats = this.catService.cats;
 }
