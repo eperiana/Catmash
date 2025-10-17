6a33b8f37269cd404c889fa8f5ce0b2575f21b33
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-17 14:21:27 +0200

    fix: maj config cors

--- api/src/main/java/fr/catmash/config/CorsConfig.java
+++ api/src/main/java/fr/catmash/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package fr.catmash.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,14 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    @Value("${spring.profiles.active:dev}")
+    private String activeProfile;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("dev".equals(activeProfile) ? "http://localhost:4200" : "https://catmash-front.onrender.com")
                         .allowedMethods("GET", "POST");
             }
         };
