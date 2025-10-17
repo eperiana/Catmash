f5490652a8de0dc740d28dd7e7440b4479decec7
Etienne PERIANAYAGASSAMY <etienne.periana@gmail.com>
2025-10-17 10:44:54 +0200

    feat: ajout chargement

--- spa/src/app/battle/battle.component.ts
+++ spa/src/app/battle/battle.component.ts
@@ -1,35 +1,44 @@
 import { Component, effect, inject, OnInit, Signal, signal } from '@angular/core';
 import { Cat } from '../models/cat';
 import { CatService } from '../services/cat.service';
+import { LoadingSpinnerComponent } from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-battle',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent],
   templateUrl: './battle.component.html',
   styleUrl: './battle.component.scss'
 })
-export class BattleComponent {
+export class BattleComponent implements OnInit {
   private catService: CatService = inject(CatService);
+
+  readonly isLoading = signal<boolean>(true);
   
   leftCat = this.catService.leftCat;
   rightCat = this.catService.rightCat;
-  isVoting = signal<boolean>(false);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1500)
+  }
+  
  
 
   vote(winner: 'left' | 'right') {
-    if (this.isVoting()) return;
-    this.isVoting.set(true);
-
     const chosen = winner === 'left' ? this.leftCat() : this.rightCat();
     if (chosen) {
       this.catService.vote(chosen.id).subscribe({
         next: () => {
-          this.isVoting.set(false);
           this.catService.pickRandomCats();
+          this.isLoading.set(true);
+          setTimeout(() => {
+            this.isLoading.set(false);
+          }, 1500)
         },
         error: () => {
-          this.isVoting.set(false);
+          console.log("API ERROR")
         }
       });
     }
